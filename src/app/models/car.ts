2b4cd9cfa852e65703466ec68397aa709397ec0a
Sevdenur <nur.bayram@bil.omu.edu.tr>
2021-03-22 12:52:03 +0300

    first commit

---
+++ src/app/models/car.ts
@@ -0,0 +1,8 @@
+export interface Car {
+    carId:number;
+    brandName:string;
+    carName:string;
+    modelYear:string;
+    colorName:string;
+    dailyPrice:number;
+}
---
+++ src/app/models/rental.ts
@@ -0,0 +1,10 @@
+export interface Rental {
+    rentalId:number;
+    brandName:string;
+    carName:string;
+    customerFirstName:string;
+    customerLastName:string;
+    companyName:string;
+    rentDate:Date;
+    returnDate:Date;
+}
