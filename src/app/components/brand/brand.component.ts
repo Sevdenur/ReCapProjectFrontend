4709886bb06741ae4635031bd84cb63c422e15f4
Sevdenur <nur.bayram@bil.omu.edu.tr>
2021-03-23 09:57:19 +0300

    changes

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -8,7 +8,9 @@ import { BrandService } from 'src/app/services/brand.service';
   styleUrls: ['./brand.component.css']
 })
 export class BrandComponent implements OnInit {
+
   brands:Brand[] = [];
+  currentBrand:Brand;
   dataLoaded=false;
   constructor(private brandService:BrandService) { }
 
@@ -22,6 +24,28 @@ export class BrandComponent implements OnInit {
       this.dataLoaded=true;
 
     })
+  }
+
+  setCurrentBrand(brand:Brand) {
+    this.currentBrand = brand;
+  }
+
+  getCurrentBrandClass(brand:Brand) {
+    if(brand==this.currentBrand) {
+      return "list-group-item active"
+    }
+    else{
+      return "list-group-item"
+    }
+  }
+
+  getAllBrandClass(){
+    if(!this.currentBrand) {
+      return "list-group-item active"
+    }
+    else{
+      return "list-group-item"
+    }
   }
 
 }
--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -2,15 +2,24 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { CarResponseModel } from '../models/carResponseModel';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Car } from '../models/car';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
-  apiUrl='https://localhost:44362/api/cars/getcardetails'
+  apiUrl='https://localhost:44362/api/'
   constructor(private httpClient:HttpClient) { }
 
   getCars():Observable<CarResponseModel>{
-    return this.httpClient.get<CarResponseModel>(this.apiUrl)
+    let newPath = this.apiUrl + "cars/getcardetails"
+    return this.httpClient.get<CarResponseModel>(newPath)
   }
+
+  getCarsByBrand(brandId:number):Observable<ListResponseModel<Car>> {
+    let newPath = this.apiUrl + "cars/getbybrandid?brandId=" + brandId
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
+  }
+
 }
